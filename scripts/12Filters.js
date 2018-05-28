1701a3dbb414fcfbc06a1925fa9202ab6bda76ff
Teja Maridu <teja.maridu@gmail.com>
2018-05-28 18:24:56 +0530

    Pratice

---
+++ scripts/12Filters.js
@@ -0,0 +1,30 @@
+
+var app = angular.module('myApp', []);
+app.controller('emp', ['$scope', 'calService', '$http', '$log', '$filter', function($scope, calService, $http, $log, $filter){
+  $scope.sal = '';
+  $scope.doSearch = function(){
+    calService.getEmpById($scope.searchEmpId, function(r){
+      $scope.empId = r.id;
+      $scope.name = r.name;
+      $scope.dob = r.dob;
+      $scope.NAME = $filter('uppercase')(r.name);
+      $scope.age = r.age;
+      $scope.sal = r.sal;
+      $scope.city = r.city;
+    });
+  }
+}]);
+
+app.service('calService', ['$http', '$log', function($http, $log){
+  // With restfull web services
+   this.getEmpById = function(empId, cb){
+     $http({
+       url: 'http://localhost:8080/webServies/resources/utils/empById?empId=' +empId,
+       method: 'GET'
+     }).then(function(resp){
+       cb(resp.data);
+     }, function(resp){
+       cb('Error occurred!!');
+     })
+   }
+}]);
