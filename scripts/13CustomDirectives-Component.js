999220401d7fa77eff2e46f5e6750f55fd671c16
Teja Maridu <teja.maridu@gmail.com>
2018-06-01 10:33:49 +0530

    changes from ofce

--- scripts/13CustomDirectives-Component.js
+++ scripts/13CustomDirectives-Component.js
@@ -6,6 +6,13 @@ app.controller('emp', ['$scope', function($scope){
 
 app.directive('myInfoMsg', function(){
   return {
+      // First way
+      // template: "<strong>This is First Message</strong>" or template: "<strong>{{msg}}</strong>"
+
+      // Second way
+      // templateUrl: "my-info-msg.html"
+
+      // Third way
       templateUrl: "my-info-msg.html"
   };
 });
