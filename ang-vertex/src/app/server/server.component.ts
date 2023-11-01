71d21bba52ab8419e88614cef5c5ac1457952a02
Teja Mr <teja.maridu@gmail.com>
2023-11-01 19:26:51 -0400

    New changes

--- ang-vertex/src/app/server/server.component.ts
+++ ang-vertex/src/app/server/server.component.ts
@@ -2,7 +2,14 @@ import { Component } from  '@angular/core'
 
 @Component({
     selector: 'app-server',
-    templateUrl: './server.component.html'
+    templateUrl: './server.component.html',
+    styleUrls: ['./server.component.css'],
+    // :TODO,,
+    // styles:['
+    //   h3 {  
+    //     color: 'bule';
+    // }
+    //  ']
 })
 export class ServerComponent {
 
