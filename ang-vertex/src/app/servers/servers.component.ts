84c76b1e19f6a119d450e530a833c6b2b0f7b8ae
Teja Mr <teja.maridu@gmail.com>
2023-11-08 22:28:38 -0500

    Components and directives

--- ang-vertex/src/app/servers/servers.component.ts
+++ ang-vertex/src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms'; 
 
 @Component({
   // selector: 'app-servers',
@@ -9,4 +10,34 @@ import { Component } from '@angular/core';
 })
 export class ServersComponent {
 
+  serverId: number = 10;
+  serverStatus: string = "OFF";
+  serverCapacity: number = 100;
+  allowNewServer: boolean = false;
+  allowBlink: boolean = false;
+  serverCreationStatus: string = "Server is not created";
+  serverCreated: boolean = false;
+  serverName: string = "TestServer";
+
+  constructor() {
+    setInterval(() => {
+      this.allowBlink = !this.allowBlink;
+    }, 500)
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 5000)
+  }
+
+  getServerCapacity(): number {
+    return this.serverCapacity;
+  }
+
+  onServerCreate() {
+    this.serverCreationStatus = "Server has been created! Name is " +this.serverName;
+    this.serverCreated = true;
+  }
+
+  onServerNameChange(event: Event){
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
 }
