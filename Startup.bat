1cf504825aca9d0bd3df1b2c248647ae970a1ed0
Teja Mr <teja.maridu@gmail.com>
2024-02-08 11:13:07 -0700

    Angular Authentication Project with all features

---
+++ Startup.bat
@@ -0,0 +1,11 @@
+@echo off
+Title 04_Open_File
+
+Start /d "C:\Program Files\Microsoft Office\root\Office16" WINWORD.EXE "D:\Notes\Java 8 Features Notes.docx"
+
+Start "D:\Notes\"
+
+
+start "C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2023.1.2\bin" idea64.exe
+
+
